2f1ed35ae6286c5ea2d948753db419ccdc321b1d
Ragna <leandro__09hotmail.com>
2023-08-19 17:41:45 -0300

    coreccion de los archivos

---
+++ src/controllers/views.controller.js
@@ -0,0 +1,49 @@
+import { productManager } from '../DAL/DAOs/productsDaos/ProductsManagerMongo.js';
+import { cartManager } from '../DAL/DAOs/cartsDaos/CartsManagerMongo.js';
+
+export const getHome = async (req, res, next) => {
+  try {
+    const products = await productManager.findAll(100, 0, undefined, undefined, undefined, true);
+    res.render('home', { products: products.docs });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getProducts = async (req, res, next) => {
+  try {
+    const { limit = 10, page = 0 } = req.query;
+    const { idCart } = req.params
+    const products = await productManager.findAll(limit, page, undefined, undefined, undefined, true);
+    const cart = await cartManager.findOneByIdPopulated(idCart)   
+    res.render('products', {
+      products: products.docs,
+      nextPage: products.nextPage,
+      prevPage: products.prevPage,
+      hasNextPage: products.hasNextPage,
+      hasPrevPage: products.hasPrevPage,
+      idCart,
+      cantCart: cart.products.length 
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getRealTimeProducts = async (req, res, next) => {
+  try {
+    const products = await productManager.findAll(100, 0, undefined, undefined, undefined, true);
+    if (req.user == undefined) {
+      res.render('realTimeProducts', { products: products.docs });
+    } else {
+      
+      res.render('realTimeProducts', { products: products.docs, firstName: req.user.firstName, cart: req.user.cart, role: req.user.role});
+    }
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getChat = async (req, res) => {
+  res.render('chat');
+};
